a1a0cef187a66f4d46c534a04f2b20fb47f041bf
George Xiong <georgexiong365@gmail.com>
2013-06-09 07:31:40 +0000

    fistr commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = 'b2e6aa51711953a5df09c22aa014913d9a6c79cef60d1c93ec339471d35d2f9fb8abc7fd7422d26027d9d24da472daf8c3556b96d18b47cea407586b146977b6'
